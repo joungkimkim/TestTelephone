f0ef630f70274f2a99ddd0b748645a28bc594742
bobo <qhrud3722@naver.com>
2023-10-25 15:02:11 +0900

    필요없어서 지웁니다.

--- src/GitTest/GitTest.java
+++
@@ -1,9 +0,0 @@
-package GitTest;
-
-import com.sun.security.jgss.GSSUtil;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!!");
-    }
-}
